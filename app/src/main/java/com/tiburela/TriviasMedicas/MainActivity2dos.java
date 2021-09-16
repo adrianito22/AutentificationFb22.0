6003aadf4ff239db6fa0b89a25758dff1e4fa7d7
adrianito22 <79810977+adrianito22@users.noreply.github.com>
2021-09-16 04:01:20 +0200

    hola22

--- app/src/main/java/com/tiburela/TriviasMedicas/MainActivity2dos.java
+++ app/src/main/java/com/tiburela/TriviasMedicas/MainActivity2dos.java
@@ -8,9 +8,6 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-
-
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
